9e362f92900db0298e32014099ebb8447a2c3199
Dayane Sallet <dayanesallet@hotmail.com>
2022-08-01 05:51:41 -0300

    Update sounds.js

--- js/sounds.js
+++ js/sounds.js
@@ -1,7 +1,7 @@
 export default function () {
   const forestAudio = new Audio('./../src/Floresta.wav')
   const rainAudio = new Audio('./../src/Chuva.wav')
-  const shopAudio = new Audio('https://github.com/dsllt/FocusTimer/blob/main/src/Cafeteria.wav?raw=true')
+  const shopAudio = new Audio('https://github.com/dsllt/FocusTimer/blob/main/src/Cafeteria.wav')
   const fireAudio = new Audio('./../src/Lareira.wav')
   const finishAudio = new Audio(
     'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
